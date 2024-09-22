23f40e39934e8936cfa3eaa7195716c04d9ceee8
Harrison Nguyen <hnguyen169@myseneca.ca>
2024-09-22 16:50:45 -0400

    added server.html

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-// include the module
 const express = require("express");
 
-// the root folder
 const path = require("path");
 
-// instantiate the express lib
 const app = express();
 
 const HTTP_PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Harrison Nguyen - 167096239');
+
+  var htmlPath = path.join(__dirname, '/views/server.html');
+
+  res.sendFile(htmlPath);
 });
 
-// listen for connections
 app.listen(HTTP_PORT, () => 
     console.log("Server listening on: http://localhost:" + HTTP_PORT));
